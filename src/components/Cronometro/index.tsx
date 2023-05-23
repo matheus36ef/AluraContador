3bc10cdb729be1ddd886b3943b93ebd2750ffc14
matheus36ef <matheusfranzonief@gmail.com>
2023-05-22 21:50:30 -0300

    começando a criar o cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,7 +2,27 @@ import Botao from "../Botao";
 import Relogio from "./Relogio";
 import Style from './Cronometro.module.scss';
 
-export default function Cronometro(){
+import {useState} from 'react'
+
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefas";
+
+
+interface Props {
+    selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro ({ selecionado } : Props){
+    /* Trabalhando aqui ----- */
+
+    const [tempo, setTempo] = useState<Number>();
+    if(selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo))
+    }
+    
+    /* Trabalhando aqui ----- */
+
+
     return(
         <div className={Style.cronometro}>
             <p className={Style.titulo}>Escolha um card e inicie o cronometro.</p>
